43aa1c8a2f4a2a7d8d1697c55e86be44e72b546d
Serhii Dzhepa <sdzhepa@magento.com>
2018-02-22 12:02:56 +0200

    lets it start

---
+++ tests/acceptance/FirstCest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class FirstCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+	$I->amOnPage('/oc-en/');
+    }
+}
